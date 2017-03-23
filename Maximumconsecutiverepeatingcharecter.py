e350924776e8fc2528d779e471d6278e0045700d
prudhvichandra <chandra.prudhvi89@gmail.com>
2017-03-22 21:53:04 -0700

    correct code

--- Maximumconsecutiverepeatingcharecter.py
+++ Maximumconsecutiverepeatingcharecter.py
@@ -1,25 +1,82 @@
-__author__ = '29146'
-import re
-word = "aaabbhhnnnnnnn"
+# inp = "abracadaaaaaabra!"  # Word/sentence to be tested
+#
+# prevchar = ""  # Previous character
+# count = 1  # Count of current character
+# maxcount = 1  # Most frequent character count
+# maxchar = ""  # Most frequent character
+#
+# for c in inp:
+#
+#     # If the character is equal to the previous
+#     if c == prevchar:
+#         count += 1
+#         # If not, check if it was bigger than the previous
+#     elif count > maxcount:
+#
+#         # Store the biggest
+#         maxcount = count
+#         maxchar = prevchar
+#
+#         # Reset the count
+#         count = 1
+#     else:
+#         # Reset the count
+#         count = 1
+#
+#     prevchar = c
+#
+# if maxchar == "":
+#     print "No consecutive characters found"
+# else:
+#     print "Most repeated character was: '%c' (%d times)" % (maxchar, maxcount)
 
-mostrepeated = ''
-count=1
-precount=1
-length=""
-for i in range(1,len(word)):
-    if word[i-1]==word[i]:
-       count+=1
+
+string = "aaaannnhhhhhiiuuuuuuuuuuugggggggggg"
+#
+# p = ""
+# max=""
+# count = 1
+# maxcount = 1
+#
+# for c in string:
+#     if c == p:
+#         count+=1
+#     elif count>maxcount:
+#         maxcount=count
+#         m = p
+#         count=1
+#     else:
+#         count = 1
+#     p =c
+#
+# print m ,maxcount
+
+p=""
+m=""
+count = 1
+max = 1
+
+for i in string:
+    if i == p:
+        count+=1
+    elif count > max:
+        max = count
+        m = p
+        count = 1
+    else:
+        count = 1
+
+    p=i
+
+print m
 
 
-    else :
 
-        count=1
-if(count>precount):
-    mostrepeated=word[i]
 
-precount=count
 
-print mostrepeated
+
+
+
 
 
 
