5ebc807bc625fc0264bf7ee2946306c2962678b7
prudhvi <prudhvi.chandra@sandisk.com>
2017-04-24 12:11:20 -0700

    find max in window

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,6 @@
 __author__ = '29146'
 
+import re
 def fibonacci(x):
 
     a = 0
@@ -13,6 +14,47 @@ def fibonacci(x):
         yield b
 
 
-for i in fibonacci(10):
-    print i
+# for i in fibonacci(10):
+#     print i
+
+def stringreverse(m):
+    a = list(m)
+    x= 0
+    y = len(a)-1
+
+    while x<y:
+        temp = a[x]
+        a[x]=a[y]
+        a[y]=temp
+        x+=1
+        y-=1
+    print ''.join(a)
+
+
+stringreverse("prudhvi")
+
+
+
+sdi = "this is prudhvi from India. who are you? prudhvi prudhvi 12345678"
+
+
+print re.findall('prudhvi',sdi)
+print re.search('from',sdi).group(0)
+
+
+name = 'pruuuudhvi'
+count = 0
+for a in name:
+    c = len(re.findall(a,name))
+    count+=c
+
+print count
+
+
+
+if count > len(name):
+    print "not unique"
+else:
+    print "unique"
+
 
